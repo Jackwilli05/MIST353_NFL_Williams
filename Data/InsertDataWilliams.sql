6c8cf9930bad5e72f4854f2bc5bbdb8f081d6a58
Jackwilli05 <jww00018@mix.wvu.edu>
2026-02-18 13:23:40 -0500

    Fixed data and added the TeamIDs

--- Data/InsertDataWilliams.sql
+++ Data/InsertDataWilliams.sql
@@ -13,72 +13,70 @@ INSERT INTO ConferenceDivision (Conference, Division) VALUES
     ('NFC', 'West');
 
 -- AFC East
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Buffalo Bills', 'Orchard Park, NY', 'Royal Blue, Red, White', 1),
-    ('Miami Dolphins', 'Miami Gardens, FL', 'Aqua, Orange, White', 1),
-    ('New England Patriots', 'Foxborough, MA', 'Navy Blue, Red, Silver', 1),
-    ('New York Jets', 'East Rutherford, NJ', 'Gotham Green, White', 1);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (1, 'Buffalo Bills', 'Orchard Park, NY', 'Royal Blue, Red, White', 1),
+    (2, 'Miami Dolphins', 'Miami Gardens, FL', 'Aqua, Orange, White', 1),
+    (3, 'New England Patriots', 'Foxborough, MA', 'Navy Blue, Red, Silver', 1),
+    (4, 'New York Jets', 'East Rutherford, NJ', 'Gotham Green, White', 1);
 GO
 
 -- AFC North
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Baltimore Ravens', 'Baltimore, MD', 'Purple, Black, Gold', 2),
-    ('Cincinnati Bengals', 'Cincinnati, OH', 'Black, Orange, White', 2),
-    ('Cleveland Browns', 'Cleveland, OH', 'Brown, Orange, White', 2),
-    ('Pittsburgh Steelers', 'Pittsburgh, PA', 'Black, Gold', 2);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (5, 'Baltimore Ravens', 'Baltimore, MD', 'Purple, Black, Gold', 2),
+    (6, 'Cincinnati Bengals', 'Cincinnati, OH', 'Black, Orange, White', 2),
+    (7, 'Cleveland Browns', 'Cleveland, OH', 'Brown, Orange, White', 2),
+    (8, 'Pittsburgh Steelers', 'Pittsburgh, PA', 'Black, Gold', 2);
 GO
 
 -- AFC South
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Houston Texans', 'Houston, TX', 'Deep Steel Blue, Battle Red, Liberty White', 3),
-    ('Indianapolis Colts', 'Indianapolis, IN', 'Royal Blue, White', 3),
-    ('Jacksonville Jaguars', 'Jacksonville, FL', 'Teal, Black, Gold', 3),
-    ('Tennessee Titans', 'Nashville, TN', 'Navy Blue, Titans Blue, Red, Silver', 3);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (9, 'Houston Texans', 'Houston, TX', 'Deep Steel Blue, Battle Red, Liberty White', 3),
+    (10, 'Indianapolis Colts', 'Indianapolis, IN', 'Royal Blue, White', 3),
+    (11, 'Jacksonville Jaguars', 'Jacksonville, FL', 'Teal, Black, Gold', 3),
+    (12, 'Tennessee Titans', 'Nashville, TN', 'Navy Blue, Titans Blue, Red, Silver', 3);
 GO
 
 --AFC West
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Denver Broncos', 'Denver, CO', 'Orange, Navy Blue, White', 4),
-    ('Kansas City Chiefs', 'Kansas City, MO', 'Red, Gold, White', 4),
-    ('Las Vegas Raiders', 'Las Vegas, NV', 'Silver, Black', 4),
-    ('Los Angeles Chargers', 'Inglewood, CA', 'Powder Blue, White, Gold', 4);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (13, 'Denver Broncos', 'Denver, CO', 'Orange, Navy Blue, White', 4),
+    (14, 'Kansas City Chiefs', 'Kansas City, MO', 'Red, Gold, White', 4),
+    (15, 'Las Vegas Raiders', 'Las Vegas, NV', 'Silver, Black', 4),
+    (16, 'Los Angeles Chargers', 'Inglewood, CA', 'Powder Blue, White, Gold', 4);
 GO
 
 --NFC East
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Dallas Cowboys', 'Arlington, TX', 'Navy Blue, Metallic Silver, White', 5),
-    ('New York Giants', 'East Rutherford, NJ', 'Royal Blue, Red, White', 5),
-    ('Philadelphia Eagles', 'Philadelphia, PA', 'Midnight Green, Silver, Black, White', 5),
-    ('Washington Commanders', 'Landover, MD', 'Burgundy, Gold', 5);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (17, 'Dallas Cowboys', 'Arlington, TX', 'Navy Blue, Metallic Silver, White', 5),
+    (18, 'New York Giants', 'East Rutherford, NJ', 'Royal Blue, Red, White', 5),
+    (19, 'Philadelphia Eagles', 'Philadelphia, PA', 'Midnight Green, Silver, Black, White', 5),
+    (20, 'Washington Commanders', 'Landover, MD', 'Burgundy, Gold', 5);
 GO
 
 --NFC North
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Chicago Bears', 'Chicago, IL', 'Navy Blue, Orange', 6),
-    ('Detroit Lions', 'Detroit, MI', 'Honolulu Blue, Silver', 6),
-    ('Green Bay Packers', 'Green Bay, WI', 'Dark Green, Gold', 6),
-    ('Minnesota Vikings', 'Minneapolis, MN', 'Purple, Gold, White', 6);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (21, 'Chicago Bears', 'Chicago, IL', 'Navy Blue, Orange', 6),
+    (22, 'Detroit Lions', 'Detroit, MI', 'Honolulu Blue, Silver', 6),
+    (23, 'Green Bay Packers', 'Green Bay, WI', 'Dark Green, Gold', 6),
+    (24, 'Minnesota Vikings', 'Minneapolis, MN', 'Purple, Gold, White', 6);
 GO
 
 -- NFC South 
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Atlanta Falcons', 'Atlanta, GA', 'Red, Black, Silver, White', 7),
-    ('Carolina Panthers', 'Charlotte, NC', 'Black, Process Blue, Silver', 7),
-    ('New Orleans Saints', 'New Orleans, LA', 'Old Gold, Black, White', 7),
-    ('Tampa Bay Buccaneers', 'Tampa, FL', 'Pewter, Red, Bay Orange, White', 7);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (25, 'Atlanta Falcons', 'Atlanta, GA', 'Red, Black, Silver, White', 7),
+    (26, 'Carolina Panthers', 'Charlotte, NC', 'Black, Process Blue, Silver', 7),
+    (27, 'New Orleans Saints', 'New Orleans, LA', 'Old Gold, Black, White', 7),
+    (28, 'Tampa Bay Buccaneers', 'Tampa, FL', 'Pewter, Red, Bay Orange, White', 7)
 GO
 
 -- NFC West
-INSERT INTO Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
-    ('Arizona Cardinals', 'Glendale, AZ', 'Cardinal Red, White, Black', 8),
-    ('Los Angeles Rams', 'Inglewood, CA', 'Royal Blue, Sol Yellow, White', 8),
-    ('San Francisco 49ers', 'Santa Clara, CA', 'Scarlet Red, Metallic Gold', 8),
-    ('Seattle Seahawks', 'Seattle, WA', 'College Navy, Action Green, Wolf Grey', 8);
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID) VALUES
+    (29, 'Arizona Cardinals', 'Glendale, AZ', 'Cardinal Red, White, Black', 8),
+    (30, 'Los Angeles Rams', 'Inglewood, CA', 'Royal Blue, Sol Yellow, White', 8),
+    (31, 'San Francisco 49ers', 'Santa Clara, CA', 'Scarlet Red, Metallic Gold', 8),
+    (32, 'Seattle Seahawks', 'Seattle, WA', 'College Navy, Action Green, Wolf Grey', 8);
 GO
 
 
-
-/*
 --See all tables
 
 SELECT TABLE_NAME 
@@ -93,7 +91,4 @@ GO
 -- See data in Team
 SELECT * FROM Team;
 GO
-*/
-
-
 
