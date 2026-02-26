3c7d3a9224d04640767bb1f62287dc3264c77b09
Jackwilli05 <jww00018@mix.wvu.edu>
2026-02-26 12:42:33 -0500

    created procedure

--- Data/DatabaseProgrammingObjectsWilliams.sql
+++ Data/DatabaseProgrammingObjectsWilliams.sql
@@ -6,22 +6,28 @@
 USE MIST353_NFL_RDB_Williams;
 GO
 
--- Query 1: ConferenceDivision table
+
+-- Query 1: 
 SELECT * FROM ConferenceDivision;
 GO
 
--- Query 2: Team table
+-- Query 2: 
 SELECT * FROM Team;
 GO
 
--- Query 3: Join query 
-SELECT 
-    t.TeamName,
-    t.TeamCityState,
-    t.TeamColors,
-    cd.Conference,
-    cd.Division
-FROM Team t
-    JOIN ConferenceDivision cd ON t.ConferenceDivisionID = cd.ConferenceDivisionID
-ORDER BY cd.Conference, cd.Division, t.TeamName;
-GO
+-- Query 3: 
+
+create or alter procedure procTeamsByConferenceDivision
+(
+    @ConferenceName NVARCHAR(50) = null,
+    @DivisionName NVARCHAR(50) = null
+)
+AS
+begin
+    select TeamName, TeamColors, Conference, Division
+    from Team T inner join ConferenceDivision C
+    on T.ConferenceDivisionID = C.ConferenceDivisionID
+    where Conference = IsNull(@ConferenceName, Conference)
+    and Division = IsNull(@DivisionName, Division)
+end
+go
