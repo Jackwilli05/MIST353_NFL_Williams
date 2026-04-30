8d692775cb7e0e81640184b70275b7cf81925912
Jackwilli05 <jww00018@mix.wvu.edu>
2026-04-30 12:09:56 -0400

    For new ui files

--- UI/nfl_playoffs_ui.py
+++ UI/nfl_playoffs_ui.py
@@ -4,12 +4,13 @@ from get_teams_in_same_conference_division_as_specified_team_ui import get_teams
 from validate_user_ui import validate_user_ui
 from get_teams_for_specified_fan_ui import get_teams_for_specified_fan_ui
 from schedule_game_ui import schedule_game_ui
+from get_all_changes_made_by_specified_admin_ui import get_all_changes_made_by_specified_admin_ui
+from get_teams_with_logos_for_specified_fan_ui import get_teams_with_logos_for_specified_fan_ui
 
 st.set_page_config(page_title="NFL Playoffs App", layout="wide")
 
 st.title("NFL Playoffs App")
 
-# Show welcome message if logged in
 if st.session_state.get("is_authenticated", False):
     st.markdown(f"### Welcome to the NFL Playoffs App, {st.session_state.app_user_fullname}!")
     st.caption("Use the sidebar to navigate through different features.")
@@ -18,12 +19,15 @@ else:
     st.caption("Please login using 'Validate User' to access all features.")
 
 st.sidebar.title("Navigation")
+
 page = st.sidebar.radio("Choose a feature", [
     "Validate User",
     "Get Teams by Conference/Division",
     "Get Teams in Same Division as Team",
     "Get Favorite Teams for Fan",
-    "Schedule Game"
+    "Get Favorite Teams with Logos",
+    "Schedule Game",
+    "View Admin Change History"
 ])
 
 if page == "Validate User":
@@ -34,11 +38,9 @@ elif page == "Get Teams in Same Division as Team":
     get_teams_in_same_conference_division_as_specified_team_ui()
 elif page == "Get Favorite Teams for Fan":
     get_teams_for_specified_fan_ui()
+elif page == "Get Favorite Teams with Logos":
+    get_teams_with_logos_for_specified_fan_ui()
 elif page == "Schedule Game":
     schedule_game_ui()
-
-st.sidebar.markdown("---")
-if st.session_state.get("is_authenticated", False):
-    st.sidebar.info(f"Logged in as: {st.session_state.app_user_fullname}\nRole: {st.session_state.user_role}")
-else:
-    st.sidebar.info("Please login using 'Validate User'")
+elif page == "View Admin Change History":
+    get_all_changes_made_by_specified_admin_ui()
